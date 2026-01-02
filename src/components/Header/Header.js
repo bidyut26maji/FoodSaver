cd4a7a4e152786630525ea309c6963ccafcb9df2
rishima17 <rishima1711@gmail.com>
2026-01-02 18:04:34 +0530

    integrated logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -38,23 +38,12 @@ const Header = () => {
   return (
     <header className={`main-header ${isSticky ? 'sticky' : ''}`}>
       <div className="header-container">
-        <div className="logo-group">
+        <Link to="/" className="logo-link">
           <div className="logo-icon">
-            <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12.0799 24L4 19.2479L9.95537 8.75216L18.04 13.4961L18.0446 4H29.9554L29.96 13.4961L38.0446 8.75216L44 19.2479L35.92 24L44 28.7521L38.0446 39.2479L29.96 34.5039L29.9554 44H18.0446L18.04 34.5039L9.95537 39.2479L4 28.7521L12.0799 24Z"
-                fill="currentColor"
-              />
-            </svg>
+            <img src="/Food-Saver logo.png" alt="FoodSaver Logo" />
           </div>
-          <h2 className="logo-text">
-            <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
-              FoodSaver
-            </Link>
-          </h2>
-        </div>
+          <h2 className="logo-text">FoodSaver</h2>
+        </Link>
 
         {/* Desktop Navigation */}
         <nav className="desktop-nav">
