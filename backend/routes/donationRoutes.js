d78a14a15da9b1ec06d29ac0ee0a5cd62d00d53c
rishima17 <rishima1711@gmail.com>
2026-01-04 15:04:44 +0530

    implemented backend functionality for restaurants

---
+++ backend/routes/donationRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const donationController = require('../controllers/donationController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Apply protection to all routes in this file
+router.use(authMiddleware.protect);
+
+router.post('/', donationController.createDonation);
+router.get('/my', donationController.getMyDonations);
+router.patch('/:id', donationController.updateDonation);
+
+module.exports = router;
