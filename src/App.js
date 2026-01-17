c81f4e95d183f171e0cce956ff688a053c503688
Priyanka Chavan <priyankachavan220406@gmail.com>
2026-01-18 03:14:18 +0530

    added cursor glow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import CursorGlow from "./components/CursorGlow/CursorGlow";
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -26,6 +27,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <CursorGlow />
       <div className="App">
         <Header />
         <main className="main-content">
